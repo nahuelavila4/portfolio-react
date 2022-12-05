3d8b0a044f6cd844488ce463c581ec9a196cfab5
Nahuel Avila <avilanahuel99@gmail.com>
2022-12-04 21:09:53 -0300

    project icons

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "../stylesheet/Project.css";
 import Boxeo from "../img/boxeo-vector.jpg";
+import {DiGithubBadge} from 'react-icons/di';
+import {FaExternalLinkAlt} from 'react-icons/fa';
+import {StyleGithub, StyleLink} from '../stylesheet/Style-Icons.js';
+
 export function Project() {
   return (
     <div className="proyectos-recientes d-flex flex-column text-center">
@@ -11,13 +15,21 @@ export function Project() {
           nec velit eget ipsum mattis euismod at sit amet dui. Ut et tortor et
           massa pellentesque fringilla at ac elit.
         </p>
-        <div className="container ">
+        <div className="container proyectos-contenedor">
           <div className="row">
             <div className="col-12 col-md-6 col-lg-4">
               <div className="proyecto">
-                <img src={Boxeo}></img>
+                <img src={Boxeo} alt="boxeo"></img>
                 <div className="overlay">
-
+                  <p>Proyecto Boxeosd df ds</p>
+                  <div className="iconos-proyectos">
+                    <a href="https://github.com/" target={"_blank"} rel="noopener noreferrer">
+                      <StyleGithub />
+                    </a>
+                    <a href="https://github.com/" target={"_blank"} rel="noopener noreferrer">
+                      <StyleLink />
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
---
+++ src/stylesheet/Style-Icons.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { DiGithubBadge } from "react-icons/di";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+export const StyleGithub = styled(DiGithubBadge)`
+  color: aliceblue;
+  font-size: 1.5rem;
+`;
+
+export const StyleLink= styled(FaExternalLinkAlt)`
+  color: aliceblue;
+  font-size: 1.5rem;
+`;
+
