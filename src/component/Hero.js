c7e38903b487a4832a5d3b51353d7a2d24173f4c
Nahuel Avila <avilanahuel99@gmail.com>
2023-01-28 11:57:59 -0300

    responsive icons

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -11,53 +11,49 @@ export function Hero() {
         </div>
       </div>
       <div className="iconos row">
-        <div className="linkedin col">
-          <div className="row">
-            <a
-              href="https://www.linkedin.com/in/nahuelavila-285803162/"
-              target={"_blank"}
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-linkedin"></i>
-              <h2>LinkedIn</h2>
-            </a>
-          </div>
+        <div className="redes linkedin col-sm-12 col-md-6 col-lg-3">
+          <a
+            href="https://www.linkedin.com/in/nahuelavila-285803162/"
+            target={"_blank"}
+            rel="noopener noreferrer"
+            // className="mx-auto"
+          >
+            <i className="bi bi-linkedin"></i>
+            <h2>LinkedIn</h2>
+          </a>
         </div>
-        <div className="github col">
-          <div className="row">
-            <a
-              href="https://github.com/nahuelavila4"
-              target={"_blank"}
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-github"></i>
-              <h2>Github</h2>
-            </a>
-          </div>
+        <div className="redes github col-sm-12 col-md-6 col-lg-3">
+          <a
+            href="https://github.com/nahuelavila4"
+            target={"_blank"}
+            rel="noopener noreferrer"
+            // className="mx-auto"
+          >
+            <i className="bi bi-github"></i>
+            <h2>Github</h2>
+          </a>
         </div>
-        <div className="mail col">
-          <div className="row">
-            <a
-              href="mailto:avilanahuel99@gmail.com"
-              target={"_blank"}
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-envelope-fill"></i>
-              <h2>Mail</h2>
-            </a>
-          </div>
+        <div className="redes mail col-sm-12 col-md-6 col-lg-3">
+          <a
+            href="mailto:avilanahuel99@gmail.com"
+            target={"_blank"}
+            rel="noopener noreferrer"
+            // className="mx-auto"
+          >
+            <i className="bi bi-envelope-fill"></i>
+            <h2>Mail</h2>
+          </a>
         </div>
-        <div className="phone col">
-          <div className="row">
-            <a
-              href="tel:1126132007"
-              target={"_blank"}
-              rel="noopener noreferrer"
-            >
-              <i className="bi bi-phone-fill"></i>
-              <h2>Phone</h2>
-            </a>
-          </div>
+        <div className="redes phone col-sm-12 col-md-6 col-lg-3">
+          <a
+            href="tel:1126132007"
+            target={"_blank"}
+            rel="noopener noreferrer"
+            // className="mx-auto"
+          >
+            <i className="bi bi-phone-fill"></i>
+            <h2>Phone</h2>
+          </a>
         </div>
       </div>
     </div>
